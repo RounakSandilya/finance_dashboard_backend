bdb69c9d40b60808bf7bb7b5aa6faeedcbabaf41
Rounak Tripathi <rounaktripathi2003@gmail.com>
2026-04-06 17:27:23 +0530

    Updated the API Dcoumentation

--- app.js
+++ app.js
@@ -4,20 +4,27 @@ const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
 const recordRoutes = require("./routes/record.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
 const app = express();
 
 //Middle ware
 app.use(cors());
 app.use(express.json()); //json parsing
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+//Swagger API
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //Routes
-app.use("/", (req,res) => {
-  res.send("Finance Dashboard root api is running....");
-})
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/records",recordRoutes);
 app.use("/api/dashboard",dashboardRoutes);
+
+app.use("/", (req,res) => {
+  res.send("Finance Dashboard root api is running....");
+})
 
 module.exports = app;
